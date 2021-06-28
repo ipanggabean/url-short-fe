7b35d593d279f24540d37cc99d9b498cfd2b7dac
igor-bonny-tua <igor-bonny.tua@allianz.com>
2021-06-29 01:52:18 +0700

    feat(redirect): Give error message for disabled link

--- src/components/Extract.js
+++ src/components/Extract.js
@@ -1,24 +1,42 @@
 import { useParams } from "react-router";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const Extract = () => {
   let { id } = useParams();
+  let [errorMessage, setErrorMessage] = useState("")
 
   const fetchOriginalLink = useCallback(() => {
     fetch(`http://localhost:8080/api/short/${encodeURIComponent(id)}`)
-      .then(response => response.json())
+      .then(response => {
+        if (!response.ok) {
+          if (response.status === 410) {
+            throw new Error("URL is already disabled")
+          }
+          throw new Error(response.status)
+        }
+        return response.json()
+      })
       .then(data => data.url)
       .then(url => window.location = url)
       .catch(error => {
+        setErrorMessage(error.toString())
         console.error('There was an error!', error);
-    });
+      });
   },[])
 
   useEffect(() => {
     fetchOriginalLink()
   }, [fetchOriginalLink])
 
-  return null
+  return (
+    <div className="container">
+      { errorMessage &&
+        <div className="alert alert-danger alert-dismissible fade show" role="alert">
+          {errorMessage}
+        </div>
+      }
+    </div>
+  )
 }
 
 export default Extract
